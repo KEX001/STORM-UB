8570221d3eb05770bf70dc7a7681d45e2836da19
ᴋᴜɴᴀʟ <154862335+KEX001@users.noreply.github.com>
2024-08-13 13:11:00 +0530

    ᴠ2.1.1 [# ᴜᴘᴅᴀᴛᴇʀ ᴀᴅᴅᴇᴅ]

---
+++ updater.py
@@ -0,0 +1,42 @@
+import asyncio
+import shlex
+import sys
+
+
+def read_requirements(file_path: str) -> list[str]:
+    with open(file_path, "r") as file:
+        return [line.strip() for line in file]
+
+
+def compare_requirements(file1: str, file2: str) -> list[str]:
+    requirements1 = set(read_requirements(file1))
+    requirements2 = set(read_requirements(file2))
+
+    return list(requirements1 ^ requirements2)
+
+
+async def runcmd(cmd: str) -> tuple[str, str, int, int]:
+    args = shlex.split(cmd)
+    process = await asyncio.create_subprocess_exec(
+        *args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    return (
+        stdout.decode("utf-8", "replace").strip(),
+        stderr.decode("utf-8", "replace").strip(),
+        process.returncode,
+        process.pid,
+    )
+
+
+async def update_requirements(deploy: str, plugins: str) -> None:
+    modules = compare_requirements(deploy, plugins)
+    try:
+        for module in modules:
+            await runcmd(f"pip install {module}")
+            print(f"ɪɴꜱᴛᴀʟʟᴇᴅ ʀᴇQᴜɪʀᴇᴍᴇɴᴛ: {module}")
+    except Exception as e:
+        print(f"ᴇʀʀᴏʀ ɪɴꜱᴛᴀʟʟɪɴɢ ʀᴇQᴜɪʀᴍᴇɴᴛꜱ: {str(e)}")
+
+
+asyncio.run(update_requirements(sys.argv[1], sys.argv[2]))
